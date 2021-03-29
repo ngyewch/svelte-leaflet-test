afe16a2638d84563510c1aba44d11b247d8a0cca
Nick Ng <ngyewch@gmail.com>
2021-03-29 17:33:18 +0800

    updated for v0.6.1

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-    import {LeafletMap, TileLayer} from 'svelte-leafletjs';
+    import {GeoJSON, LeafletMap, TileLayer} from 'svelte-leafletjs';
 
     const mapOptions = {
         center: [1.250111, 103.830933],
@@ -12,10 +12,20 @@
         maxNativeZoom: 19,
         attribution: "© OpenStreetMap contributors",
     };
+    const geoJsonOptions = {
+        style: function(geoJsonFeature) {
+            console.log('style', geoJsonFeature);
+            return {};
+        },
+        onEachFeature: function(feature, layer) {
+            console.log('onEachFeature', feature, layer);
+        },
+    };
 </script>
 
 <div class="example" style="width: 100%; height: 100%;">
     <LeafletMap options={mapOptions}>
         <TileLayer url={tileUrl} options={tileLayerOptions}/>
+        <GeoJSON url="example.geojson" options={geoJsonOptions}/>
     </LeafletMap>
 </div>
